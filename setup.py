54946fd30134335c8764a5eab694e0d717e26a3e
Andreas Heger <andreas.heger@gmail.com>
2018-09-28 11:52:12 +0100

    {AH} update deps

--- setup.py
+++ setup.py
@@ -1,31 +1,6 @@
-import sysconfig
 import sys
-import os
-import subprocess
-import re
 import setuptools
 from setuptools import setup, find_packages
-
-########################################################################
-#######################################################################
-# Check for dependencies
-#
-# Is there a way to do this more elegantly?
-#     1. Run "pip install numpy"
-#     2. Wrap inside functions (works for numpy/pysam, but not cython)
-try:
-    import numpy
-except ImportError:
-    raise ImportError(
-        "the CGAT code collection requires numpy to be installed "
-        "before running setup.py (pip install numpy)")
-
-try:
-    import pysam
-except ImportError:
-    raise ImportError(
-        "the CGAT code collection requires pysam to "
-        "be installed before running setup.py (pip install pysam)")
 
 from distutils.version import LooseVersion
 if LooseVersion(setuptools.__version__) < LooseVersion('1.1'):
@@ -33,12 +8,6 @@ if LooseVersion(setuptools.__version__) < LooseVersion('1.1'):
     raise ImportError(
         "the CGAT code collection requires setuptools 1.1 higher")
 
-########################################################################
-########################################################################
-IS_OSX = sys.platform == 'darwin'
-
-########################################################################
-########################################################################
 # collect version
 sys.path.insert(0, "daisy")
 from version import __version__ as daisy_version
@@ -46,8 +15,6 @@ from version import __version__ as daisy_version
 daisy_packages = find_packages()
 daisy_package_dirs = {'daisy': 'daisy'}
 
-##########################################################
-##########################################################
 # Classifiers
 classifiers = """
 Development Status :: 3 - Alpha
@@ -66,7 +33,7 @@ setup(
     # package information
     name='daisy',
     version=daisy_version,
-    description='daisy : data',
+    description='daisy : data isomething system',
     author='Andreas Heger',
     author_email='andreas.heger@gmail.com',
     license="MIT",
@@ -75,14 +42,13 @@ setup(
     long_description='daisy : data ',
     classifiers=[_f for _f in classifiers.split("\n") if _f],
     url="http://www.cgat.org/cgat/Tools/",
-    # package contents
     packages=daisy_packages,
     package_dir=daisy_package_dirs,
     include_package_data=True,
     entry_points={
-        'console_scripts': ['daisy = daisy.tools.cli:main']
+        'console_scripts': ['daisy = daisy.tools.cli:main',
+                            'cgat-bench = daisy.tools.cli:main']
     },
-    # other options
     zip_safe=False,
     test_suite="tests",
 )
